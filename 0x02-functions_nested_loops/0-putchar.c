f74068b7dfff3a5053d3cdc314a000877d5857e5
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 12:42:09 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
-int main(void) {
+/**
+* Prints the word '_putchar'
+* Adds a new line to the end of the file
+**/
+int main(void) 
+{
 	char s[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int length = 8;
 	int i = 0;
-	while(i < length) {
+	while(i < length) 
+	{
 		_putchar(s[i]);
 		i++;
 	}
+	_putchar('\n');
 	return 0;
 }
