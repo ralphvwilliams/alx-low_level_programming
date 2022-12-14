a3e896ccd6c45a195c3952141bd30f52e137a0bd
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 14:12:12 +0000

    Add files via upload

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ * Return: Always 0
+*/
+void print_alphabet_x10(void)
+{
+    int i = 0, j;
+
+    while (i < 10)
+    {
+        j = 97;
+        while (j < 123)
+        {
+            _putchar(j);
+            j++;
+        }
+        _putchar('\n');
+        i++;
+    }
+}
