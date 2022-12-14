567759ec92fa67311292118f7615ea5fc4799967
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 16:34:42 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,19 +8,19 @@
 
 void jack_bauer(void)
 {
-	int hr;
-	int mn;
+int hr;
+int mn;
 
-	for (hr = 0; hr < 24; hr++)
-	{
-		for (mn = 0; mn < 60; mn++)
-		{
-			_putchar((hr / 10) + '0');
-			_putchar((hr % 10) + '0');
-			_putchar(':');
-			_putchar((mn / 10) + '0');
-			_putchar((mn % 10) + '0');
-			_putchar('\n');
-		}
-	}
+for (hr = 0; hr < 24; hr++)
+{
+for (mn = 0; mn < 60; mn++)
+{
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((mn / 10) + '0');
+_putchar((mn % 10) + '0');
+_putchar('\n');
+}
+}
 }
