f2e7f5078e4e94d9ae85a0219f83cea96b14fd53
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 13:04:26 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
 **/
 void print_alphabet(void)
 {
-  int i = 97;
+int i = 97;
 
-  while (i < 123)
-  {
-    _putchar(i);
-    i++;
-  }
-  _putchar('\n');
+while (i < 123)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
 }
 
