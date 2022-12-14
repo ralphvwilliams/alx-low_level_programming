3dbe12a7d638209bfb5b417b79c107db60641e31
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 13:00:33 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* main - prints lowercase alphabets from a - z
+* print_alphabet - prints lowercase alphabets from a - z
 * Return: Always 0
 **/
-int main(void)
+void print_alphabet(void)
 {
   int i = 97;
   
