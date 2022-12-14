5dda744fd00e2edb9547af39dcd04dfbb441cf2d
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 14:29:39 +0000

    Add files via upload

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is an alphabet
+ * @c: Character to check
+ * Return: 1 if alphabet, 0 if not
+*/
+int _isalpha(int c)
+{
+if (c > 64 && c < 123)
+return (1);
+else
+return (0);
+}
