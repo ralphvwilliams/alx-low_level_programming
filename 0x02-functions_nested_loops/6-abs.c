62451498c336ee4cb4e0d01a7ed42fbe7c875a88
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 16:02:28 +0000

    Add files via upload

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of a number
+ * @n: - number to be checked
+ * Return: Absolute value of n
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (n * -1);
+else
+return (n);
+}
